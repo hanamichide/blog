7e35a20c40a11f1acde771da79bf84caf3c3c779
hanamichide <greco.federico1993@gmail.com>
2023-11-12 12:04:01 +0100

    fin es java

--- jscript.js
+++ jscript.js
@@ -2,17 +2,14 @@ let isTransitionActive = false;
 
 function startTransition() {
     let overlay = document.getElementById('overlay');
-    overlay.classList.remove("none");
+    overlay.classList.add("active");
     let body = document.body;
+   
     if (!isTransitionActive) {
         
         let maxWidth = window.innerWidth;
-
-        
         overlay.style.width = maxWidth + 'px';
-        
-        body.classList.add('no-scroll')
-        
+        body.classList.add('no-scroll');
         isTransitionActive = true;
 
     
@@ -27,15 +24,13 @@ function reverseTransition() {
     if (isTransitionActive) {
         
         overlay.style.width = '0';
-       
-
-        body.classList.remove('no-scroll')
+        body.classList.remove('no-scroll');
         isTransitionActive = false;
     }
     setTimeout(function(){
-        overlay.classList.add("none");
-    },700);
+        overlay.classList.remove("active");
+    },900);
 
-    body.classList.remove('no-scroll')
+    
 }
 
