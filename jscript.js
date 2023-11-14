49e84cb03a0cc41993e6ddbf9922c6998ed5a031
hanamichide <greco.federico1993@gmail.com>
2023-11-14 03:02:34 +0100

    finita

--- jscript.js
+++ jscript.js
@@ -2,6 +2,7 @@ let isTransitionActive = false;
 
 function startTransition() {
     let overlay = document.getElementById('overlay');
+    let hiddenElements = document.querySelectorAll('.hidden');
     overlay.classList.add("active");
     let body = document.body;
    
@@ -11,26 +12,34 @@ function startTransition() {
         overlay.style.width = maxWidth + 'px';
         body.classList.add('no-scroll');
         isTransitionActive = true;
-
+        hiddenElements.forEach(element => {
+            element.classList.add('active');
+        });
     
     }
 }
 
 function reverseTransition() {
     let overlay = document.getElementById('overlay');
-    let body = document.body;
+    let hiddenElements = document.querySelectorAll('.hidden');
+        let body = document.body;
     
 
     if (isTransitionActive) {
         
         overlay.style.width = '0';
-        body.classList.remove('no-scroll');
+        ;
         isTransitionActive = false;
     }
     setTimeout(function(){
         overlay.classList.remove("active");
+        body.classList.remove('no-scroll')
     },900);
 
+    hiddenElements.forEach(element => {
+        element.classList.remove('active');
+    });
+    900;
     
 }
 
