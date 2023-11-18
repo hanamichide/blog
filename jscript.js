cc9a7b57b93aa6837f6b977870297c615926c1cb
hanamichide <greco.federico1993@gmail.com>
2023-11-18 21:30:34 +0100

    finito viewmore

--- jscript.js
+++ jscript.js
@@ -20,6 +20,7 @@ function startTransition() {
     }
 }
 
+
 function reverseTransition() {
     let overlay = document.getElementById('overlay');
     let hiddenElements = document.querySelectorAll('.hidden');
@@ -43,5 +44,75 @@ function reverseTransition() {
     900;
     
     
-}
+};
+
+
+import {posts} from './card.js';
+
+function printPosts() {
+
+    const sectionPost = document.getElementById("second-section");
+
+    if (sectionPost === null) {
+        return
+    }
+
+    for (let i = 0; i < posts.length; i++) {
+        
+        const post = printPost(posts[i]);
+
+        sectionPost.appendChild(post);
+
+    }
+
+};
+//generare post//
+document.addEventListener("DOMContentLoaded", function() {
+    printPosts();
+ });
+ 
+ function printPost (data) {
+    const post = document.createElement("div");
+    
+   
+       post.innerHTML =
+       `
+       <div class="post">
+        <h6  class="title-post">
+           ${data.title}
+        </h6>
+        <span class="post-date">${data.publishedAt}</span>
+        <p>
+           ${data.preview}
+        </p>
+        <div class="category">
+           <div class="first-cat">${data.categories[0]}</div>
+           <div class="second-cat">${data.categories[1]}</div>
+        </div>
+        <div class="comments">
+        <div class="mph"><i class="fa-regular fa-message mp"></i>3</div>
+           <div class="view-more"><a class="ex1" href="viewmore.html?id=${data.id}" id="viewmore">View More</a></div>
+        </div>
+
+       </div>
+       `
+       return post;
+    }
+        
+
+  //tasto indietro Logo//
+    const logoElement = document.getElementById('logo');
+
+if (logoElement) {
+    logoElement.addEventListener('click', function() {
+        window.history.back();
+    });
+} else {
+    
+};
+
+
+
+
+
 
---
+++ viewmore.js
@@ -0,0 +1,36 @@
+import {posts} from './card.js';
+
+function printBody() {
+
+    const viewMore = document.getElementById("viewmore");
+
+    const params = new URLSearchParams(window.location.search);
+    const id =parseInt(params.get("id"));
+
+    console.log(id);
+
+    let data = posts.find(x => x.id === id);
+
+    console.log(data);
+
+    if(!data){
+        return
+    }
+
+    const elemTitle = document.getElementById("title");
+
+    if(elemTitle){
+        elemTitle.innerHTML = data.title;
+    }
+
+    const elemContent = document.getElementById("content");
+    if(elemContent){
+        elemContent.innerHTML = data.body;
+    }
+
+    
+  
+
+}
+
+printBody();
